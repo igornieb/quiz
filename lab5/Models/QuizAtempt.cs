15ce78f8a64b1f2bb357c8d28ef6625af5803c18
Igor <66256669+igornieb@users.noreply.github.com>
2023-06-03 12:13:22 +0200

    Delete lab5 directory

--- lab5/Models/QuizAtempt.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace lab5.Models
-{
-    internal class QuizAtempt
-    {
-        public long Id { get; set; }
-        public string PlayerName { get; set; }
-        public int Score { get; set; }
-    }
-}
